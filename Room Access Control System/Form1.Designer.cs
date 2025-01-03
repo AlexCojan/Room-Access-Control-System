c492ac186ec567045aa05db82c0db6755f2dd404
Alex_Cojan <115487269+AlexCojan@users.noreply.github.com>
2025-01-03 16:04:59 +0000

    Some Labelling

--- Room Access Control System/Form1.Designer.cs
+++ Room Access Control System/Form1.Designer.cs
@@ -76,6 +76,9 @@
             this.VisitorGuest = new System.Windows.Forms.Button();
             this.EmergencyResponder = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BuildingApha = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -520,18 +523,49 @@
             // dateTimePicker1
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.CustomFormat = "hh:mm MM/dd/yyyy";
+            this.dateTimePicker1.CustomFormat = "hh:mm tt MM/dd/yyyy";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker1.Location = new System.Drawing.Point(1220, 80);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(145, 20);
             this.dateTimePicker1.TabIndex = 38;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Alpha Teaching Room";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(899, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Beta Teaching Room";
+            // 
+            // BuildingApha
+            // 
+            this.BuildingApha.AutoSize = true;
+            this.BuildingApha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BuildingApha.Location = new System.Drawing.Point(59, 237);
+            this.BuildingApha.Name = "BuildingApha";
+            this.BuildingApha.Size = new System.Drawing.Size(158, 25);
+            this.BuildingApha.TabIndex = 41;
+            this.BuildingApha.Text = "Building Apha";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.BuildingApha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.EmergencyResponder);
             this.Controls.Add(this.button5);
@@ -638,5 +672,8 @@
         private System.Windows.Forms.Button VisitorGuest;
         private System.Windows.Forms.Button EmergencyResponder;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label BuildingApha;
     }
 }
